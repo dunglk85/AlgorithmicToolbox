1e35c664cb059c372b10d6f5cde4e6197d8f7ef0
lekimdung <dunglk85@gmail.com>
2022-09-08 22:16:09 +0700

    majority element

--- Divide-and-Conquer/Majority Element/majority_element.py
+++ Divide-and-Conquer/Majority Element/majority_element.py
@@ -8,11 +8,24 @@ def majority_element_naive(elements):
             return 1
 
     return 0
-
-
+def majority_element_rec(elements, lo, hi):
+    if lo == hi:
+        return elements[lo]
+    else:
+        mid = (lo+hi)//2
+        left = majority_element_rec(elements, lo, mid)
+        right = majority_element_rec(elements, mid+1, hi)
+        if left == right:
+            return left
+        left_count = sum(1 for i in range(lo, hi+1) if elements[i] == left)
+        right_count = sum(1 for i in range(lo, hi + 1) if elements[i] == right)
+        return left if left_count > right_count else right
 def majority_element(elements):
     assert len(elements) <= 10 ** 5
-    type here
+    lo = 0
+    hi = len(elements) - 1
+    el = majority_element_rec(elements, lo, hi)
+    return 1 if 2*elements.count(el) > hi+1 else 0
 
 
 if __name__ == '__main__':
